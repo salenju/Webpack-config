5de413d2bca8ad60c18fcb44025f50e7f11de8ef
云生流长 <31212648+salenju@users.noreply.github.com>
2022-02-22 21:03:33 +0800

    feat: 使用html-webpack-plugin

---
+++ 03-自动引入资源/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,  // 清空上次打包的文件
+  },
+
+  mode: 'none',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'app.html', // 入口html文件名
+      inject: 'body',  // 在哪里引入js文件
+    })
+  ],
+}
